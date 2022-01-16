a438d95ff49422fba6c1d33288c1eb2f8f4d4ac5
Preetham Ganesh <preetham.ganesh2015@gmail.com>
2022-01-15 18:39:07 -0600

    added retrieve_image_names

--- codes/data_preprocessing.py
+++ codes/data_preprocessing.py
@@ -30,14 +30,38 @@ def load_json_file(file_name: str):
         Returns:
             Loaded JSON file which contains the image file names and captions in the current split of the dataset.
     """
-    directory_path = '../data/original_data/annotations/'
+    directory_path = '../data/original_data/annotations'
     with open('{}/{}'.format(directory_path, file_name), 'r') as f:
         captions = json.load(f)
     return captions
 
 
+def retrieve_image_names(data_split: str):
+    """Retrieves image names in the current data split directory.
+
+        Args:
+            data_split: String which contains the data split name i.e. train or val.
+
+        Returns:
+            List of image names with their corresponding path.
+    """
+    directory_path = '../data/original_data'
+    image_names = []
+
+    # Iterates across image names in the current data split directory.
+    for i in os.listdir('{}/{}2017'.format(directory_path, data_split)):
+
+        # Checks if the current image name is classified as file. If yes, then appended to the final list of images.
+        if os.path.isfile('{}/{}2017/{}'.format(directory_path, data_split, i)):
+            image_names.append('{}/{}2017/{}'.format(directory_path, data_split, i))
+            
+    return image_names
+
+
 def main():
-    x = 0
+    original_validation_captions = load_json_file('captions_val2017.json')
+    original_validation_image_names = retrieve_image_names('val')
+    print(original_validation_image_names)
 
 
 if __name__ == '__main__':
